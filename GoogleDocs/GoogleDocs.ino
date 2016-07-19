ec3d2e9ba19777db62c2490aa37ef3c874c5340d
Rodolfo Cossovich <todocono@gmail.com>
2016-07-19 17:10:25 +0800

    works with WS2812B led rainbow

--- GoogleDocs/GoogleDocs.ino
+++ GoogleDocs/GoogleDocs.ino
@@ -12,6 +12,7 @@
     This example is in public domain.
 */
 
+#include <Adafruit_NeoPixel.h>
 
 #define D0 16     //onboard LED 
 #define D1 5
@@ -23,8 +24,10 @@
 #define D7 13
 #define D8 15   //needs to be open when flashing
 
+#define PIN D2
 #include <ESP8266WiFi.h>
 #include "HTTPSRedirect.h"
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN, NEO_GRB + NEO_KHZ800);
 
 // Replace with your network credentials
 const char* ssid = "TPE-Hackerspace";
@@ -70,6 +73,10 @@ String urlClose = String("/macros/s/") + GScriptId + "/exec?value=0";
 
 
 void setup() {
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
   Serial.begin(9600);
   Serial.println();
   Serial.print("Connecting to wifi: ");
@@ -150,7 +157,7 @@ void loop() {
   newState = digitalRead(D1);
   Serial.print( "read a: " );
   Serial.println( newState );
-  
+
   // We use D1 as an input to read wether a switch has been flipped or not
   // Here could be the right spot to expand into RFID or other behaviors
   if (newState != oldState ) {
@@ -161,6 +168,8 @@ void loop() {
 
     String url = String("/macros/s/") + GScriptId + "/exec?value=" + userID;
     client.printRedir(url, host, googleRedirHost);
+
+  
   }
   else {
     digitalWrite( D0, HIGH); // switches OFF the LED
@@ -169,9 +178,100 @@ void loop() {
     //client.printRedir(urlClose, host, googleRedirHost);
   }
 
-
+  if ( newState ) {
+      rainbow(20);
+    }
+    else{
+      colorWipe(strip.Color(0, 0, 0), 50); // turns them off
+    }
   // this is note from Sujay, the original author. We didn't register any reboots on NodeMCU:
   // In my testing on a ESP-01, a delay of less than 1500 resulted
   // in a crash and reboot after about 50 loop runs.
-  delay(1500);
+  delay(1000);
+}
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
+    for (int q = 0; q < 3; q++) {
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, Wheel( (i + j) % 255)); //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
